de414a8b3ba1243cbe454f5e7bfbd5ab00b5669d
user.email : "hamza2203c@aptechgdn.net <hamza2203c@aptechgdn.net>
2026-04-14 11:55:58 +0500

    add react js file

---
+++ code/react js/myFirstProject/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+  return (
+    <div>
+
+   <h1>Muhammad Hamza | Learning React JS</h1>
+    <h2>today is our first react js class</h2>
+    
+    </div>
+  )
+}
+
+export default App
